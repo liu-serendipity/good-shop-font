1e9cb79ca055e058e1d1ff3cf7b733089ec473e1
刘利民 <liulimin@dushu365.com>
2023-04-07 18:19:18 +0800

    feat:us

--- good-shop-font-antd/src/views/us/index.tsx
+++ good-shop-font-antd/src/views/us/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { NavBar } from 'antd-mobile';
+import { Box, Center } from '@/components';
+import { useNavigate } from 'react-router-dom';
 
 const Us = () => {
-  return <div className=''>Us</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className='us'>
+      <Box pos={'fixed'} w='100%'>
+        <NavBar onBack={() => navigate(-1)}>关于我们</NavBar>
+      </Box>
+      <Center pt='0.45rem' w='100%'>
+        <p>作者：LLM</p>
+      </Center>
+    </div>
+  );
 };
 
 export default Us;

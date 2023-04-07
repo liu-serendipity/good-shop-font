86703b50818366ee26d3d35cbf635de1d19511ff
刘利民 <liulimin@dushu365.com>
2023-04-07 11:48:37 +0800

    feat:client404

--- good-shop-font-antd/src/views/client404/index.tsx
+++ good-shop-font-antd/src/views/client404/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Button } from 'antd-mobile';
+import { Box, Text, Center } from '@/components';
+import { useNavigate } from 'react-router-dom';
+
+import bg404 from './imgs/bg404.webp';
 
 const Client404 = () => {
-  return <div className=''>Client404</div>;
+  const navigate = useNavigate();
+  return (
+    <div className='client404' style={{ width: '100%', height: '100vh' }}>
+      <Center w='100%' h='100%'>
+        <Box>
+          <img src={bg404} />
+          <Box m='0.2rem auto'>
+            <Button color='primary' fill='outline' size='mini' onClick={() => navigate('/home')}>
+              <Text c='#1677ff' fz='0.12rem'>
+                返回首页
+              </Text>
+            </Button>
+          </Box>
+        </Box>
+      </Center>
+    </div>
+  );
 };
 
 export default Client404;
